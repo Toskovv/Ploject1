a3e0cd78b2de8119d76ba4e3c2c49984c3fcb655
Toskovv <stu2113811070@uni-plovdiv.bg>
2025-01-24 00:13:10 +0200

    +

--- Ploject1/Ploject1/Program.cs
+++ Ploject1/Ploject1/Program.cs
@@ -2,6 +2,7 @@ using CarStore.BL.Interfaces;
 using CarStore.BL.Services;
 using CarStore.DL.Interfaces;
 using CarStore.DL.Repositories;
+using Microsoft.AspNetCore.Builder;
 
 namespace Ploject1
 {
@@ -9,23 +10,29 @@ namespace Ploject1
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
 
+            var builder = WebApplication.CreateBuilder(args);
+           
             // Add services to the container.
             builder.Services.AddScoped<ICarService, CarService>();
             builder.Services.AddScoped<ICarRepository, CarRepository>();
-
             builder.Services.AddControllers();
 
-            var app = builder.Build();
+            // Add Swagger services
+            builder.Services.AddSwaggerGen();
 
+            var app = builder.Build();
+            if (app.Environment.IsDevelopment())
+            {
+                //app.UseSwagger();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+
+            }
             // Configure the HTTP request pipeline.
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
 
             app.Run();
